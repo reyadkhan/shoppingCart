7fcb7da9fae45b7552694d2a2374f9023a57bc35
Reyad Khan <reyadk2@gmail.com>
2020-08-10 02:44:44 +0600

    Cart alias is added in composer.json file instead for service provider

--- src/ShoppingCartServiceProvider.php
+++ src/ShoppingCartServiceProvider.php
@@ -3,9 +3,7 @@
 namespace WebApp\ShoppingCart;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Foundation\AliasLoader;
 use WebApp\ShoppingCart\Contracts\Cart;
-use WebApp\ShoppingCart\Facades\Cart as CartFacade;
 
 class ShoppingCartServiceProvider extends ServiceProvider
 {
@@ -28,8 +26,6 @@ class ShoppingCartServiceProvider extends ServiceProvider
         $this->mergeConfigFrom(__DIR__ . '/../config/cart.php', 'cart');
 
         $this->app->bind('cart', CartManager::class);
-
-        AliasLoader::getInstance()->alias('Cart', CartFacade::class);
     }
 
     /**
